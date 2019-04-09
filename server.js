c6f1d4151428a0eb26cc134b676e34740d37d196
Glitch (pumped-roll) <none>
2019-04-09 10:06:26 +0000

    🍔🙋 Checkpoint ./server.js:3298818/237

--- server.js
+++ server.js
@@ -3,6 +3,9 @@
 var express = require('express');
 var cors = require('cors');
 
+var multer  = require('multer')
+var upload = multer({ dest: 'uploads/' })
+
 // require and use "multer"...
 
 var app = express();
@@ -17,6 +20,15 @@ app.get('/', function (req, res) {
 app.get('/hello', function(req, res){
   res.json({greetings: "Hello, API"});
 });
+
+app.post('/api/fileanalyze', upload.single('upfile') , async(req, res)=>{
+  try{
+    
+  }
+  catch(err){
+    errror: err.message
+  }
+})
 
 app.listen(process.env.PORT || 3000, function () {
   console.log('Node.js listening ...');
